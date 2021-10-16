c5e52260ec95888c3189ab2c9dd6791bf2e22ff9
Elliott <elliottwink@hotmail.com>
2021-10-16 16:38:51 -0500

    updated gui and how much shuffling is done

--- SortingAlgorithms/HelperFunctions/ListShuffle.py
+++ SortingAlgorithms/HelperFunctions/ListShuffle.py
@@ -5,9 +5,10 @@ import random
 
 def ListShuffle(list):
     l = list
-    for i in range(len(l) - 1):
-        p1 = random.randrange(0,(len(l)))
-        p2 = random.randrange(0,(len(l)))
-        l1 = l[p1]
-        l[p1] = l[p2]
-        l[p2] = l1
+    for i in range(2):
+        for i in range(len(l) - 1):
+            p1 = random.randrange(0,(len(l)))
+            p2 = random.randrange(0,(len(l)))
+            l1 = l[p1]
+            l[p1] = l[p2]
+            l[p2] = l1
